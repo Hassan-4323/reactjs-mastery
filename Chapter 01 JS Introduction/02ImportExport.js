43ff0ae6ba0fc5df31335d1a66561d3fa0b47b56
Hassan Jan <hassanzahir2004@gmail.com>
2026-06-19 23:56:31 -0700

    Initial React learning journey setup

---
+++ Chapter 01 JS Introduction/02ImportExport.js
@@ -0,0 +1,10 @@
+// default export
+
+import chacha from './02ImportExportApp.js'
+
+console.log(chacha);
+
+// named export
+import { namedarr } from './02ImportExportApp.js';
+
+console.log(namedarr);
---
+++ Chapter 01 JS Introduction/02ImportExportApp.js
@@ -0,0 +1,9 @@
+// default export
+const arr = [20,30,40,50];
+
+export default arr;
+
+// name export
+
+export const namedarr = [11,12,13,14,15];
+
