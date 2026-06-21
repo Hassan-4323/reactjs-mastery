d580f62bbc2ef9ce5207f4de424396ff44d8dd65
Hassan Jan <hassanzahir2004@gmail.com>
2026-06-20 23:33:27 -0700

    Chapter 03 Components

---
+++ Chapter-03-Components/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <div className="card">
+        <h1>Hello Hassan Jan</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+      </div>
+    </div>
+  )
+}
+
+export default App
