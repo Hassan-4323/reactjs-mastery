c1757017adb5284b3d046d4c58925fbca4cc2284
Hassan Jan <hassanzahir2004@gmail.com>
2026-06-26 11:31:03 -0700

    props in advance

--- chapter-04-props/src/App.jsx
+++ chapter-04-props/src/App.jsx
@@ -4,8 +4,8 @@ import Card from './components/Card'
 const App = () => {
   return (
     <div className='parent'>
-      <Card/>
-      <Card/>
+      <Card user='Hassan' age = {22} img='https://images.unsplash.com/photo-1698758004450-1f9da080f9b4?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDIyfENEd3V3WEpBYkV3fHxlbnwwfHx8fHw%3D'/>
+      <Card user='Jan' age = {21} img='https://plus.unsplash.com/premium_photo-1781023149311-056a89533d7d?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDV8Q0R3dXdYSkFiRXd8fGVufDB8fHx8fA%3D%3D'/>
     </div>
   )
 }
--- chapter-04-props/src/components/Card.jsx
+++ chapter-04-props/src/components/Card.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 
-const Card = () => {
+const Card = (props) => {
+  console.log(props);
   return (
     <div className="card">
-            <img src="https://images.unsplash.com/photo-1698758004450-1f9da080f9b4?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDIyfENEd3V3WEpBYkV3fHxlbnwwfHx8fHw%3D" alt="" />
-            <h1>Hassan Jan</h1>
+            <img src={props.img} alt="" />
+            <h1>{props.user}</h1>
             <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ut, voluptates.
             </p>
             <button>View Profile</button>
