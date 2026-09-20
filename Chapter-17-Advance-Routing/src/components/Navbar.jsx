09f13ba366e9246ae6f823032dadfa4e0263bfcc
Hassan Jan <hassanzahir2004@gmail.com>
2026-09-20 09:41:51 -0700

    Dynamic routing

--- Chapter-17-Advance-Routing/src/components/Navbar.jsx
+++ Chapter-17-Advance-Routing/src/components/Navbar.jsx
@@ -11,6 +11,7 @@ const Navbar = () => {
         < Link className='text-lg font-semibold' to='/'>Home</Link>
         < Link className='text-lg font-semibold' to='/about' >About</Link>
         <Link className='text-lg font-semibold' to='/contact' >Contact</Link>
+        <Link className='text-lg font-semibold' to='/courses'>Courses</Link>
 
       </div>
 
